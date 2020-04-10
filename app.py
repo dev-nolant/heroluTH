fd93512a0874dbcc6ad2fddd4936946ec4c09480
nolan taft <nolantgaming@gmail.com>
2020-04-10 13:15:55 -0600

    first

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 from flask_restful import Api
 from resources.todo import Todo
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(Todo, "/todo/<int:id>")
-
+@app.route("/")
+def home():
+    return render_template("htmlss.html", content="Testing", x=4)
 if __name__ == "__main__":
   app.run()
