9206218f448ba895a59dd2f34a10c3345d8dcb90
nolan taft <nolantgaming@gmail.com>
2020-04-09 19:07:06 -0600

    test

---
+++ resources/todo.py
@@ -0,0 +1,24 @@
+from flask_restful import Resource
+
+todos = [
+  {
+    "id": 'Y2r92kPq1pabNayjsMdX208vyH+j78gUyDkd6qN+u9k=',
+    "item": "Welcome :)",
+    "status": "End Game"
+  }
+]
+
+class Todo(Resource):
+  def get(self, id):
+    for todo in todos:
+      if(id == todo["id"]):
+        return todo, 200
+    return "Item not found for the id: {}".format(id), 404
+
+  def put(self, id):
+    for todo in todos:
+      if(id == todo["id"]):
+        todo["item"] = request.form["data"]
+        todo["status"] = "Open"
+        return todo, 200
+    return "Item not found for the id: {}".format(id), 404
